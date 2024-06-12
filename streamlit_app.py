5a4fd0778c68bdf0a509247a5312b2835c86e5cb
Darryl Williams <39777038+njasharp@users.noreply.github.com>
2024-06-11 21:45:00 -0700

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,11 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import graphviz
+#import graphviz
 import plotly.figure_factory as ff
-from bokeh.plotting import figure
+#from bokeh.plotting import figure
 
-st.balloons()
+#st.balloons()
 st.markdown("# Data Evaluation App")
 
 st.title('web 3')
@@ -53,4 +53,4 @@ p = figure(
 
 p.line(x, y, legend_label='Trend', line_width=2)
 
-st.bokeh_chart(p, use_container_width=True)
+#st.bokeh_chart(p, use_container_width=True)
